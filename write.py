8e2a03a0d56f60fc5c642aee3bf7f3e55fc2d140
c1pr14n055 <ciprianosss@gmail.com>
2016-12-12 16:55:17 +0200

    1-3 models

--- write.py
+++ write.py
@@ -11,32 +11,135 @@ rand_coords = ["44º39'12\"N 26º12'4\"E", "44º39'12.59\"N 26º12'3.09\"E",
 			"44º39'11.49\"N 26º11'59.61\"E", "44º39'10.99\"N 26º11'58.68\"E",
 			"44º39'10.44\"N 26º12'57.21\"E", "44º39'9.95\"N 26º12'55.67\"E"]
 
-specs_five = [
+specs_one = [
+	"#2c2012",					#color
+	"20",						#<= 14 ch font-size
+	"16",						#<= 20 ch font-size
+	"12",						#> 20 ch font-size
+	"fonts/BebasNeue.otf",		#font-familly
+	"0x0",						#rotation
+	"+0",						#left
+	"-5"						#top
+]
+
+specs_two = [
+	"#05060b",					#color
+	"18",						#<= 14 ch font-size
+	"14",						#<= 20 ch font-size
+	"10",						#> 20 ch font-size
+	"fonts/BebasNeue.otf",		#font-familly
+	"2x2",						#rotation
+	"-30",						#left
+	"+10"						#top
+]
+
+specs_three = [
+	"#2c2012",					#color
+	"16",						#<= 14 ch font-size
+	"12",						#<= 20 ch font-size
+	"9",						#> 20 ch font-size
+	"fonts/BebasNeue.otf",		#font-familly
+	"2x2",						#rotation
+	"+85",						#left
+	"+22"						#top
+]
+
+specs_four = [
 	"#2c2012",					#color
 	"24",						#<= 14 ch font-size
 	"20",						#<= 20 ch font-size
-	"16",						#>= 25 ch font-size
+	"16",						#> 20 ch font-size
 	"fonts/BebasNeue.otf",		#font-familly
 	"4.5x4.5",					#rotation
 	"+87",						#left
 	"-10"						#top
 ]
 
+specs_five = [
+	"#2c2012",					#color
+	"24",						#<= 14 ch font-size
+	"20",						#<= 20 ch font-size
+	"16",						#> 20 ch font-size
+	"fonts/BebasNeue.otf",		#font-familly
+	"4.5x4.5",					#rotation
+	"+87",						#left
+	"-10"						#top
+]
+
+specs_six = [
+	"#2c2012",					#color
+	"24",						#<= 14 ch font-size
+	"20",						#<= 20 ch font-size
+	"16",						#> 20 ch font-size
+	"fonts/BebasNeue.otf",		#font-familly
+	"4.5x4.5",					#rotation
+	"+87",						#left
+	"-10"						#top
+]
+
+specs_seven = [
+	"#2c2012",					#color
+	"24",						#<= 14 ch font-size
+	"20",						#<= 20 ch font-size
+	"16",						#> 20 ch font-size
+	"fonts/BebasNeue.otf",		#font-familly
+	"4.5x4.5",					#rotation
+	"+87",						#left
+	"-10"						#top
+]
+
+specs_eight = [
+	"#2c2012",					#color
+	"24",						#<= 14 ch font-size
+	"20",						#<= 20 ch font-size
+	"16",						#> 20 ch font-size
+	"fonts/BebasNeue.otf",		#font-familly
+	"4.5x4.5",					#rotation
+	"+87",						#left
+	"-10"						#top
+]
+
+specs_nine = [
+	"#2c2012",					#color
+	"24",						#<= 14 ch font-size
+	"20",						#<= 20 ch font-size
+	"16",						#> 20 ch font-size
+	"fonts/BebasNeue.otf",		#font-familly
+	"4.5x4.5",					#rotation
+	"+87",						#left
+	"-10"						#top
+]
+
+specs_ten = [
+	"#2c2012",					#color
+	"24",						#<= 14 ch font-size
+	"20",						#<= 20 ch font-size
+	"16",						#> 20 ch font-size
+	"fonts/BebasNeue.otf",		#font-familly
+	"4.5x4.5",					#rotation
+	"+87",						#left
+	"-10"						#top
+]
+
+specs_list = [specs_one, specs_two, specs_three, specs_four, specs_five, 
+			specs_six, specs_seven, specs_eight, specs_nine, specs_ten]
+
 def write_on_image(text):
-	which = random.randint(1, 10)
+	which = 3 #random.randint(1, 10)
 	coord = rand_coords[which - 1]
+	specs = specs_list[which - 1]
 
 	if len(text) <= 14:
-		font_size = specs_five[1]
+		font_size = specs[1]
 	elif len(text) <= 20:
-		font_size = specs_five[2]
+		font_size = specs[2]
 	elif len(text) > 20:
-		font_size = specs_five[3]
+		font_size = specs[3]
 
-	subprocess.call(["convert source_pics/5.jpg -gravity center -fill '" + specs_five[0] 
+	subprocess.call(["convert source_pics/" + str(which) + ".jpg -gravity center -fill '" + specs[0] 
 		+ "' -pointsize " + font_size 
-		+ " -font " + specs_five[4] 
-		+ " -annotate " + specs_five[5] + specs_five[6] + specs_five[7] + " " + pipes.quote(text) 
+		+ " -font " + specs[4] 
+		+ " -annotate " + specs[5] + specs[6] + specs[7] + " " + pipes.quote(text) 
 		+ " -fill '#f1f1f1'"
 		+ " -pointsize 14"
 		+ " -font fonts/MyriadPro.otf"
@@ -52,7 +155,8 @@ def __init__():
 	#write_on_image("\n".join(wrap("HOLLEMAN SPECIAL TRANSPORT & PROJECT CARGO", 25)))
 	#.strip()
 
-	if not True:
+	if True:
+		write_on_image("\n".join(wrap("x", 25))) # x
 		write_on_image("\n".join(wrap("HANDSOME ADV", 25))) # 12 ch
 		write_on_image("\n".join(wrap("HANDSOME ADVSS", 25))) # 14 ch
 		write_on_image("\n".join(wrap("HANDSOME ADVSSXXX", 25))) # 18 ch
